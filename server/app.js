1f35187dd6c25731de1f501c67fdcd970edf6ec0
shahid ahmad dar <darshahid45ds@gmail.com>
2026-05-18 11:53:28 +0530

    Configure dynamic backend Socket.IO CORS to support local network mobile testing

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: (origin, callback) => callback(null, true),
         methods: ["GET", "POST"],
         credentials: true
     },
